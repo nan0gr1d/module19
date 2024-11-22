7a03ad6112e21cc6d507ddc92a34b53f6179cffc
Urban <prestofan@gmail.com>
2024-11-22 21:17:22 +0300

    Исправил модели согласно замечаниям

--- urbanprj/task1/models.py
+++ urbanprj/task1/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Buyer(models.Model):
     name = models.CharField(max_length=30)
     balance = models.DecimalField(decimal_places=6, max_digits=10)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
 
 
 class Game(models.Model):
     title = models.CharField(max_length=30)
     cost = models.DecimalField(decimal_places=6, max_digits=10)
     size = models.DecimalField(decimal_places=6, max_digits=10)
-    description = models.CharField(max_length=100)
-    age_limited = models.BooleanField()
+    description = models.TextField()
+    age_limited = models.BooleanField(default="False")
     buyer = models.ManyToManyField(Buyer, related_name='games')
 
